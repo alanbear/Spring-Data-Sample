1dbc8329ec2bfa7b5ff6be34f753044aaf73fc1d
alanbear <alan.hsieh@104.com.tw>
2017-11-30 17:06:46 +0800

    add sql comment format sql output

--- src/main/java/alanbear/sample/spring/data/repository/HumanRepository.java
+++ src/main/java/alanbear/sample/spring/data/repository/HumanRepository.java
@@ -1,16 +1,19 @@
 package alanbear.sample.spring.data.repository;
 
+import static org.hibernate.jpa.QueryHints.HINT_COMMENT;
+
 import alanbear.sample.spring.data.model.Human;
+import java.util.Collection;
+import java.util.List;
+import javax.persistence.QueryHint;
 import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import java.util.Collection;
-import java.util.List;
 
 @CacheConfig(cacheNames = "humans")
 @Repository
@@ -25,6 +28,7 @@ public interface HumanRepository extends BaseRepository<Human, String> {
     List<Human> findBySex(String sex);
 
     //by page
+    @QueryHints(value = { @QueryHint(name = HINT_COMMENT, value = "a query for pageable")})
     Page<Human> findBySex(String sex, Pageable pageable);
 
     //by order by Asc
